4b315afdbdf781078b9065e7c497ac854dd2148f
TOMÁS KALINSCKY <47494132@ort>
2024-07-04 09:27:24 -0300

    pasado agregar item a react

---
+++ src/components/DivagregarItem/AgregarItem.js
@@ -0,0 +1,74 @@
+import './AgregarItem.css';
+import React, { useState } from 'react';
+
+const AgregarItem = () => {
+    const [tareas, setTareas] = useState([]);
+    const [tareaInput, setTareaInput] = useState('');
+
+    const handleInputChange = (e) => {
+        setTareaInput(e.target.value);
+    };
+
+    const mandar = () => {
+        if (tareaInput === "") {
+            alert("No puedes enviar una tarea vacía");
+        } else {
+            const nuevaTarea = {
+                tarea: tareaInput,
+                tachada: false,
+                fechaCreacion: new Date(),
+                fechaTachado: null
+            };
+            setTareas([...tareas, nuevaTarea]);
+            setTareaInput('');
+        }
+    };
+
+    const tachar = (index) => {
+        const nuevasTareas = [...tareas];
+        nuevasTareas[index].tachada = !nuevasTareas[index].tachada;
+        setTareas(nuevasTareas);
+    };
+
+    const eliminarTarea = (index) => {
+        const nuevasTareas = tareas.filter((_, i) => i !== index);
+        setTareas(nuevasTareas);
+    };
+
+    return (
+        <div>
+            <h1>To Do List</h1>
+            <div id="agregar-tarea">
+                <div className="input-container">
+                    <input 
+                        type="text" 
+                        id="agregado" 
+                        value={tareaInput} 
+                        onChange={handleInputChange}
+                    />
+                    <button id="send" onClick={mandar}>Agregar Item</button>
+                </div>
+            </div>
+            <ul id="listToDO">
+                {tareas.map((t, index) => (
+                    <li key={index}>
+                        <span 
+                            className="tarea" 
+                            onClick={() => tachar(index)}
+                            style={{ textDecoration: t.tachada ? 'line-through' : 'none' }}
+                        >
+                            {t.tarea}
+                        </span>
+                        <button className="bot" onClick={() => eliminarTarea(index)}>Eliminar</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default AgregarItem;
+
+
+
+
